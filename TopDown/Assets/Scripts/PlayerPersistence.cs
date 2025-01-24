b0a68375dd16bcb8d006d22613950173131f0e79
BSmith099 <bdsmith099@gmail.com>
2025-01-24 14:24:05 -0600

    Saving Money Player Data

---
+++ TopDown/Assets/Scripts/PlayerPersistence.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public static class PlayerPersistence
+{
+    public static void SaveData(GameManager gameManager)
+    {
+        PlayerPrefs.SetInt("money", gameManager.score + gameManager.PlayerData.Money);
+    }
+
+    public static PlayerData LoadData()
+    {
+        int money = PlayerPrefs.GetInt("money");
+
+        PlayerData playerData = new PlayerData()
+        {
+            Money = money
+        };
+
+        return playerData;
+    }
+}
